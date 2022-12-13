97e15045ed940f901d992cd8f854326989d21f53
Florian Hassanen <florian.hassanen@gmail.com>
2022-12-13 10:28:58 +0100

    Install a router for the patient-list and the practitioner-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PatientFormComponent } from './patient-form/patient-form.component';
 import { PatientListComponent } from './patient-list/patient-list.component';
 import { PractitionerListComponent } from './practitioner-list/practitioner-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,17 @@ import { PractitionerListComponent } from './practitioner-list/practitioner-list
     PatientListComponent,
     PractitionerListComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: 'patients', component: PatientListComponent },
+      { path: 'practitioners', component: PractitionerListComponent },
+      { path: '', redirectTo: '/patients', pathMatch: 'full' },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
